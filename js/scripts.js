f0fc1bf73fab13036588b74e8fa86855aeae7cce
Hyewon Cho, Meria Thomas and Skye Nguyen <meria@>
2018-08-15 14:08:23 -0700

    add backend logic to convert to decimal

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,32 @@
+var numberCheck = function(number) {
+  var numberString = number.toString();
+  if (!numberString.match(/^[01]+$/)) {
+        alert("Please enter a binary number!");
+        return -1;
+  }
+  else {
+    return conversion(number);
+  }
+}
+
+var conversion = function (number){
+  var result = 0;
+  var string = number.toString().split("").reverse();
+  for( var i = 0; i<string.length; i++){
+    result += parseInt(string[i] * Math.pow(2,i));
+  }
+  return result;
+}
+
 
 $(document).ready(function(){
   $("#input").submit(function(event){
     event.preventDefault();
 
     var inputNumber = parseInt($("#inputNumber").val());
-  })
+    $("#result").empty();
+    if (numberCheck(inputNumber) !== -1) {
+      $("#result").append(numberCheck(inputNumber));
+    }
+  });
 });
